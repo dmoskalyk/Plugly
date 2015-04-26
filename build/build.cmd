5f76187619bc9a94a74739deff2135c7e8539c79
Dmitry <lokiworld@gmail.com>
2015-04-26 12:41:54 +0300

    NuGet

---
+++ build/build.cmd
@@ -0,0 +1,5 @@
+@echo off
+set BuildProps=Configuration=Release;SignAssembly=true;DelaySign=false;AssemblyOriginatorKeyFile=..\..\..\lokiworld.snk
+call nuget pack ..\src\Plugly\Plugly.csproj -Build -p %BuildProps%
+call nuget pack ..\src\Plugly.Unity\Plugly.Unity.csproj -Build -p %BuildProps%
+pause
---
+++ build/nuget.cmd
@@ -0,0 +1,1 @@
+..\src\.nuget\NuGet.exe %*
